6fb4968d2259353bffd59c6bca3ff424fbd42baa
mangopanda455 <gfielding26@scarsdaleschools.org>
2022-11-17 12:55:17 -0500

    first commit

---
+++ appy_wappy/appy wappy/appy_wappyApp.swift
@@ -0,0 +1,18 @@
+//
+//  appy_wappyApp.swift
+//  appy wappy
+//
+//  Created by Graham Fielding on 11/17/22.
+//
+//
+
+import SwiftUI
+
+@main
+struct appy wappyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
